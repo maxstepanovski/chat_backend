94b29f8dd271fae1e3ceec35bd05157a7aa17283
Максим <stepanov_maks@mail.ru>
2021-03-31 18:21:08 +0300

    application extension

--- src/main/kotlin/com/example/demo/AuthApplication.kt
+++ src/main/kotlin/com/example/demo/AuthApplication.kt
@@ -2,9 +2,10 @@ package com.example.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
-class AuthApplication
+class AuthApplication: SpringBootServletInitializer()
 
 fun main(args: Array<String>) {
     runApplication<AuthApplication>(*args)
