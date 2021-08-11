e18d899c922cfe61be03fef4015e831a0a3b35f1
Alsu Stepanova <shaykhattarova.alsu@gmail.com>
2021-08-11 21:58:00 +0300

    dependencies update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.3.0.RELEASE"
-    id("io.spring.dependency-management") version "1.0.9.RELEASE"
+    id("org.springframework.boot") version "2.5.3"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
     war
-    kotlin("jvm") version "1.3.72"
-    kotlin("plugin.spring") version "1.3.72"
+    kotlin("jvm") version "1.5.21"
+    kotlin("plugin.spring") version "1.5.21"
+    kotlin("plugin.jpa") version "1.5.21"
 }
 
 group = "com.example"
@@ -23,7 +24,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.security:spring-security-test")
-    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
--- src/main/kotlin/com/example/demo/BeanConfiguration.kt
+++ src/main/kotlin/com/example/demo/BeanConfiguration.kt
@@ -7,9 +7,10 @@ import com.example.demo.domain.MainInteractor
 import io.jsonwebtoken.SignatureAlgorithm
 import io.jsonwebtoken.security.Keys
 import org.postgresql.ds.PGSimpleDataSource
-import org.springframework.beans.factory.annotation.Value
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.Environment
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import javax.crypto.SecretKey
@@ -18,45 +19,43 @@ import javax.sql.DataSource
 @Configuration
 class BeanConfiguration {
 
-    @Value("fcm.file.path")
-    private lateinit var fcmFilePath: String
+    @Autowired
+    lateinit var env: Environment
 
     @Bean
     fun passwordEncoder() = BCryptPasswordEncoder()
 
     @Bean
-    fun dataSource(): DataSource {
-        val dataSource = PGSimpleDataSource()
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/postgres")
-        dataSource.user = "postgres"
-        dataSource.password = "123456"
-        return dataSource
+    fun dataSource(): DataSource = PGSimpleDataSource().apply {
+        setUrl(env.getProperty("spring.datasource.url"))
+        user = env.getProperty("spring.datasource.username")
+        password = env.getProperty("spring.datasource.password")
     }
 
     @Bean
     fun authRepository(
-            passwordEncoder: PasswordEncoder,
-            authorityRepository: AuthorityRepository,
-            userRepository: UserRepository
+        passwordEncoder: PasswordEncoder,
+        authorityRepository: AuthorityRepository,
+        userRepository: UserRepository
     ) = AuthInteractor(passwordEncoder, authorityRepository, userRepository)
 
     @Bean
     fun mainRepository(
-            fcmInteractor: FcmInteractor,
-            conversationRepository: ConversationRepository,
-            conversationMessageRepository: ConversationMessageRepository,
-            messageRepository: MessageRepository,
-            userRepository: UserRepository,
-            userConversationRepository: UserConversationRepository,
-            userFirebaseTokenRepository: UserFirebaseTokenRepository
+        fcmInteractor: FcmInteractor,
+        conversationRepository: ConversationRepository,
+        conversationMessageRepository: ConversationMessageRepository,
+        messageRepository: MessageRepository,
+        userRepository: UserRepository,
+        userConversationRepository: UserConversationRepository,
+        userFirebaseTokenRepository: UserFirebaseTokenRepository
     ) = MainInteractor(
-            fcmInteractor,
-            conversationRepository,
-            userConversationRepository,
-            messageRepository,
-            conversationMessageRepository,
-            userRepository,
-            userFirebaseTokenRepository
+        fcmInteractor,
+        conversationRepository,
+        userConversationRepository,
+        messageRepository,
+        conversationMessageRepository,
+        userRepository,
+        userFirebaseTokenRepository
     )
 
     @Bean
